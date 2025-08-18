1a07d368887ae21e111f03c03340ad11abffa59c
sanju marik <jackiehsj32@gmail.com>
2025-08-18 22:25:26 +0530

    changes 2

--- routes/user.route.js
+++ routes/user.route.js
@@ -15,7 +15,7 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/verify", verifyUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/current", getCurrentUser);
+userRouter.post("/current", getCurrentUser);
 userRouter.post("/logout", logoutUser);
 
 // Story limit decrement
