c2ce7788da8f80b341cae59fae16bc753a624dbf
sanju marik <jackiehsj32@gmail.com>
2025-08-20 00:21:15 +0530

    cors origin change to vercel

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-storyteller-frontend.vercel.app/",
     credentials: true
 }))
 
