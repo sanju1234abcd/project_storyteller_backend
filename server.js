1d820c21570267ff5bac4db918f2c27bf7283d61
sanju marik <jackiehsj32@gmail.com>
2025-08-20 00:24:46 +0530

    slash change in cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://virtual-storyteller-frontend.vercel.app/",
+    origin: "https://virtual-storyteller-frontend.vercel.app",
     credentials: true
 }))
 
